89f4ee28979a95d8a747464dd2bb35bde28d7e28
PawqanBadghare <pawanbadghare8@gmail.com>
2023-01-03 03:41:47 -0800

    3 january 2023 changes

--- src/component/Pawanjsx.jsx
+++ src/component/Pawanjsx.jsx
@@ -8,11 +8,16 @@ function Pawan() {
     <sub>-subscript text</sub>
     <small>-small text</small>
     <del>-deleted text</del>
-    <p> rich dad poor dad book bigerst seller book in world</p>
-    <h1>css is course in html 2022</h1>
+    <p> rich dad poor dad book bigerst seller book in world <b>robet.t kiyushaki</b></p>
+    <h1 style={{color: "blue"}}>css is course in html 2022</h1>
     <h2>-css is course in html 2022</h2>
     <h3>-css is course in html 2022</h3>
-
+     <h4>-css is course in html 2020</h4>
+     <h5>-css is course in html 2022</h5>
+     <h6>-css is course in html2022</h6>
+     <p>H <sub>2</sub> O</p>
+     <p>x<sup>2</sup>+V</p>
+     <button>onClick Event Listener</button>
   </div>
   )
     }
