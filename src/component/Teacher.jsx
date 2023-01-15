c241293c093dfe8d23aa03bc4deec6ade7aae361
PawqanBadghare <pawanbadghare8@gmail.com>
2023-01-15 03:26:37 -0800

    html change img

---
+++ src/component/Teacher.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../component/style/Teacher.css';
+import Dog from '../assets/One.jpg'
+const Teacher = () => {
+  return (
+    <>
+    <div className='teacher-div'>
+     <h1>Center</h1>
+     <img src={Dog} alt='img'/>
+     <img src={Dog} alt='img'/>
+    </div>
+    <div className='card-div'>
+        <p></p>
+<div className='button-div'>
+<button></button>
+<button></button>
+</div>
+<img />
+    </div>
+    
+    </>
+  )
+}
+
+export default Teacher
