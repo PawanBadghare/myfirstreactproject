6d673cf9761754aade07e4f6cd8510b9b1ee5418
PawqanBadghare <pawanbadghare8@gmail.com>
2023-01-16 05:21:57 -0800

    HEADING

---
+++ src/component/style/Pbadhare.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Pbadhare = () => {
+  return (
+    <div>
+  <h1> V.R.PRODUCTS</h1>
+  <h2>V.R.PRODUCTS</h2>
+  <button>on click</button>
+  <sup>V.R.PRODUCTS</sup>
+
+    </div>
+  )
+}
+
+export default Pbadhare
+
+
+
+
+
