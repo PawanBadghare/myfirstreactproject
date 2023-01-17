85ddba652b88c96bc62f444afb46665a72c8ad54
PawqanBadghare <pawanbadghare8@gmail.com>
2023-01-17 09:40:10 -0800

    del some word jsx

---
+++ src/component/style/sachin.jsx
@@ -0,0 +1,15 @@
+import React from 'react't
+
+const sachin = () => {
+  return (
+    <div>
+    <p>sachin is best cricke</p>
+    <del>sachin</del>
+    <sup>modal is best sachin</sup>
+    <h1>gross profit is market</h1>    
+    </div>
+  )
+}
+
+export default sachin  
+
