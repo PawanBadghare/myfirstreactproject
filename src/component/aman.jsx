aa520d9a18f3bb67b767bebb72d75f0349e5fef3
PawqanBadghare <pawanbadghare8@gmail.com>
2023-01-19 09:46:20 -0800

    aman.jsx

---
+++ src/component/aman.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const aman = () => {
+  return (
+    <div>
+    <p>componet style assets</p>
+    <h1> file new open</h1>
+    <button>on click</button>    
+    </div>
+  )
+}
+
+export default aman
