2d10875a5e1ab31d823ef944ecaff0b93564914f
PawqanBadghare <pawanbadghare8@gmail.com>
2023-01-19 09:36:45 -0800

    jsx del some word

--- src/component/style/dada.jsx
+++ src/component/style/dada.jsx
@@ -6,7 +6,8 @@ const sachin = () => {
     <p>sachin is best cricke</p>
     <del>sachin</del>
     <sup>modal is best sachin</sup>
-    <h1>gross profit is market</h1>    
+    <h1>gross profit is market</h1>
+    <del> modal is best sachin</del>    
     </div>
   )
 }
